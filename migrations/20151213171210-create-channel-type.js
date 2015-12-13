a4bdaf6561aedf5c12dd0d81e0122640df220c85
Adam McCarthy <amccarthy@mail.rit.edu>
2015-12-13 17:18:49 -0500

    Add sequelize and create configuration and models

---
+++ migrations/20151213171210-create-channel-type.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('ChannelTypes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      service: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('ChannelTypes');
+  }
+};
